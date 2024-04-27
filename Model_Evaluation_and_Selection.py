11e0a4835bde0e675114e96a7e049c470d80e184
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-04-27 22:52:49 +0530

    Week 2 Complete

---
+++ Model_Evaluation_and_Selection.py
@@ -0,0 +1,35 @@
+# for array computations and loading data
+import numpy as np
+
+# for building linear regression models and preparing data
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# for building and training neural networks
+import tensorflow as tf
+
+# custom functions
+import utils
+
+# reduce display precision on numpy arrays
+np.set_printoptions(precision=2)
+
+# suppress warnings
+tf.get_logger().setLevel('ERROR')
+tf.autograph.set_verbosity(0)
+
+# Load the dataset from the text file
+data = np.loadtxt('./data/data_w3_ex1.csv', delimiter=',')
+
+# Split the inputs and outputs into separate arrays
+x = data[:,0]
+y = data[:,1]
+
+# Convert 1-D arrays into 2-D because the commands later will require it
+x = np.expand_dims(x, axis=1)
+y = np.expand_dims(y, axis=1)
+
+print(f"the shape of the inputs x is: {x.shape}")
+print(f"the shape of the targets y is: {y.shape}")
